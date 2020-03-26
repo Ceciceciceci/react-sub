42755b9ea57f929b5faf818818810b10bbc3ccb3
Ceci Tran <ceci@Cecis-MacBook-Pro.local>
2020-03-26 16:58:06 -0700

    put command

--- react-subscription/api/server.js
+++ react-subscription/api/server.js
@@ -82,15 +82,5 @@ app.put('/api/current', (req, res, next) => {
     // res.send('Subscription is updated');
 });
 
-// app.get('/api/preview', (req, res)=>{
-//     let showPlan = {
-//         plan:  currPlan.plan,
-//         name:  currPlan.name,
-//         seats: currPlan.seats,
-//         cost: currPlan.seats * currPlan.cost      
-//       }
-//       res.json(showPlan);
-// })
-
 const port = 9000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
